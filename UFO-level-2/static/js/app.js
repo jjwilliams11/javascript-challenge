3f081b0e24a355c7ab28db8958421f50a54f25af
JJ Williams <74847091+jjwilliams11@users.noreply.github.com>
2021-03-26 10:53:27 -0400

    level 2 initial table and values store, filter needs work

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,9 +1,104 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select table body
+let table = d3.select("tbody");
 
-tableData
+//Read in tableData
+tableData.forEach((sighting) => {
+    var row = table.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+// Select the buttons
+let filterButton = d3.select("#filter-button");
+let clearButton = d3.select("#clear-button")
+
+// Select the form
+let form = d3.select("#search-form");
+
+
+// Create event handlers 
+filterButton.on("click", runFilter);
+clearButton.on("click", runClear)
+form.on("submit",runFilter);
+
+// Form Filter Function
+function runFilter(){
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the Date Element
+    // let dateElement = d3.select("#datetime-input");
+    let dateValue = d3.select("#datetime-input").property("value");
+    let cityValue = d3.select("#city-input").property("value");
+    let stateValue = d3.select("#state-input").property("value");
+    let countryValue = d3.select("#country-input").property("value");
+    let shapeValue = d3.select("#shape-input").property("value");
+
+    console.log()
+
+    // Select the Date Value Entered
+    // let dateValue = dateElement.property("value");
+
+    // Filter table by Date Value
+    // let dateSearch = tableData.filter(tableData => tableData.datetime === dateValue);
+    let filterSearch = tableData.filter(data => {
+        if (data.datetime === dateValue || data.city === cityValue ||
+            data.state === stateValue || data.country ===countryValue ||
+            data.shape === shapeValue)
+            return data;
+            console.log(data)
+        }
+    )
+
+    // Show filter results in console
+    // console.log(dateSearch);
+
+    // Clear table before input results
+    // table.html("")
+
+    // // Input the filter data into table format
+    // dateSearch.forEach((results) => {
+    //     row = table.append("tr");
+    //     Object.entries(results).forEach(([key,value]) => {
+    //         row.append("td").text(results.datetime);
+    //         row.append("td").text(results.city);
+    //         row.append("td").text(results.state);
+    //         row.append("td").text(results.country);
+    //         row.append("td").text(results.shape);
+    //     });
+    
+    // });
+
+}
+
+// Clear Filter Function
+function runClear(){
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Clear table before input results
+    table.html("")
+    
+
+    // Input the filter data into table format
+    tableData.forEach((sighting) => {
+        var row = table.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+
+    // Clear input text
+    
+}
 
 
 function search(data){
