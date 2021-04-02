dbbe0a682b5c8b2e0e00d3cdf800387dbd894f5b
JJ Williams <74847091+jjwilliams11@users.noreply.github.com>
2021-04-02 19:50:55 -0400

    Bonus search now working even with empty input cells

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -6,6 +6,8 @@ let table = d3.select("tbody");
 
 //Create function to read in tableData
 function fullTable(data){
+    table.html("");
+
     tableData.forEach((sighting) => {
         var row = table.append("tr");
         Object.entries(sighting).forEach(([key, value]) => {
@@ -30,18 +32,8 @@ clearButton.on("click", runClear);
 form.on("submit",runFilter);
 
 
-// Eliminate null functions
-function removeNull(test){
-    if (test === ""){
-        test.disabled = true
-    }
-    else{
-        test = test
-    }
-}
 
-// Form Filter Function
-
+// Create Filter Function
 function runFilter(){
 
     // Prevent the page from refreshing
@@ -55,46 +47,56 @@ function runFilter(){
     let countryValue = d3.select("#country-input").property("value");
     let shapeValue = d3.select("#shape-input").property("value");
 
-    // let dateValue = if (d3.select("#datetime-input").property("value") !=){
-    //     return d3.select("#city-input").property("value");
-    //     }
-    //     else{
-    //         d3.select("#city-input").property("value").disabled = true
-    //     }
 
+    // Establish array of input filters to search
+    let filters = [];
 
-    let searchResults  = tableData.filter((data) => {
-        if (data.datetime === dateValue && data.city === cityValue &&
-            data.state === stateValue && data.country === countryValue &&
-            data.shape === shapeValue)
-            return data;
-    })
-
-
-    // let dateResults = tableData.filter(data => {
-    //     if(dateValue !== ""){
-    //         data.datetime === dateValue
-    //     }
-    //     else 
-    // })
-    // searchResults = tableData.filter(data => {
-    //     if(cityValue !== ""){
-    //         data.city === cityValue
-    //     }
-    // })
+    if (dateValue.length>0){
+        filters.push(dateValue)
+    }
+    if (cityValue.length>0){
+        filters.push(cityValue)
+    }
+    if (stateValue.length>0){
+        filters.push(stateValue)
+    }
+    if (countryValue.length>0){
+        filters.push(countryValue)
+    }
+    if (shapeValue.length>0){
+        filters.push(shapeValue)
+    }
+    
 
     //  // Clear table before input results
-     table.html("");
+    table.html("");
 
+    // Establish array a returned rows from filter after looping through input values
+    let searchData = []
 
-     searchResults.forEach((results) => {
-         row = table.append("tr");
-         Object.entries(results).forEach(([key,value]) => {
-         cell = row.append("td").text(value);
-         })
+    filters.forEach(val => {
+        test = val
+        searchData = tableData.filter(row => {
+            if (row.datetime === test || row.city === test ||
+                row.state === test || row.country === test ||
+                row.shape === test)
+                return row; 
+        })    
     })
-    console.log(searchResults)
-     
+            
+    // Append table with only search results
+    searchData.forEach((sighting) => {
+        row = table.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+        cell = row.append("td");
+        cell.text(value);
+        });
+    });
+
+    // Display results to compare
+    console.log(searchData);
+    console.log(filters)
+
 
 }
 
@@ -108,119 +110,9 @@ function runClear(){
     table.html("");
     
 
-    // Input the filter data into table format
-    tableData.forEach((sighting) => {
-        var row = table.append("tr");
-        Object.entries(sighting).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
-        });
-      });
+    // Restablish full table of sightings
+    fullTable(tableData);
 
-    // Clear input text
 };
 
-
 fullTable(tableData);
-
-// Form Filter Function
-// function runFilter(){
-
-//     // Prevent the page from refreshing
-//     d3.event.preventDefault();
-
-//     // Select the Search Values
-//     // let dateElement = d3.select("#datetime-input");
-//     // if (d3.select("#datetime-input").property("value") !== null){
-//     //     dateValue = d3.select("#datetime-input").property("value")
-//     // }
-//     // if (d3.select("#city-input").property("value") !== null){
-//     //     cityValue = d3.select("#city-input").property("value")
-//     // }
-
-    
-//     let dateValue = d3.select("#datetime-input").property("value");
-//     let cityValue = d3.select("#city-input").property("value");
-//     let stateValue = d3.select("#state-input").property("value");
-//     let countryValue = d3.select("#country-input").property("value");
-//     let shapeValue = d3.select("#shape-input").property("value");
-
-   
-    // let searchResults = tableData.filter(data => {
-    //    if(dateValue !== ""){
-    //        data.datetime === dateValue
-    //    }
-       
-    // let searchResults = tableData.filter(datum => datum.datetime === dateValue);
-//    });
-//    let citySearch = dateSearch.filter(data => data.city === cityValue);
-//    let stateSearch = 
-
-    //    if(cityValue !== null){
-    //        return data.city === cityValue;
-    //    }
-    // console.log(searchData);
-    // console.log(cityValue);
-    // searchData = filterData.filter(tableData.datetime === dateValue)
-
-    // let searchData = tableData.filter(tableData => tableData.datetime === dateValue);
-        
-        // && tableData.city === cityValue &&
-        // tableData.state === stateValue && tableData.country === countryValue && tableData.shape === shapeValue)
-            
-    
-    
-    // Filter table by Date Value
-    // let searchResults = tableData.filter((data) => {
-    //     if(dateValue !== null)
-    //         {data.datetime === dateValue}
-    //     else{} 
-    //     && 
-    //     if(cityValue !== null)
-    //         {data.city === cityValue}
-    // });
-    // || data.state === stateValue || data.country === countryValue ||
-    // data.shape === shapeValue)
-
-
-    // Clear table before input results
-//     table.html("");
-
-
-//     searchResults.forEach((results) => {
-//         row = table.append("tr");
-//         Object.entries(results).forEach(([key,value]) => {
-//         cell = row.append("td").text(value);
-//         });
-    
-//     });
-        
-
-//     // console.log(dateSearch);
-
-
-//     })
-// }
-//     )
-
-    // Show filter results in console
-    // console.log(dateSearch);
-
-    // Clear table before input results
-    // table.html("")
-
-//     // Input the filter data into table format
-    // dateSearch.forEach((results) => {
-    //     row = table.append("tr");
-    //     Object.entries(results).forEach(([key,value]) => {
-    //         row.append("td").text(results.datetime);
-    //         row.append("td").text(results.city);
-    //         row.append("td").text(results.state);
-    //         row.append("td").text(results.country);
-    //         row.append("td").text(results.shape);
-    //     });
-    
-    // });
-
-// }
-
